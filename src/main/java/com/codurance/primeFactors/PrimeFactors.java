685e7cd137fd2d10d753a0ae919b53aa632c8cc3
Tom <thomas.spencer@codurance.com>
2021-09-14 20:55:37 +0100

    add refactoring

--- src/main/java/com/codurance/primeFactors/PrimeFactors.java
+++ src/main/java/com/codurance/primeFactors/PrimeFactors.java
@@ -8,14 +8,10 @@ import java.util.List;
 public class PrimeFactors {
   public static List<Integer> of(int n) {
     final List<Integer> factors = Lists.newArrayList();
-    int divisor = 2;
-    while (n > 1) {
-      while (n % divisor == 0){
+    for (int divisor = 2; n > 1; divisor++) {
+      for (; n % divisor == 0; n /= divisor){
         factors.add(divisor);
-        n /= divisor;
       }
-
-      divisor++;
     }
 
     return factors;
