4fb13dd981f3d98ee185addc1f9cfd4b5ea65e09
Tom <thomas.spencer@codurance.com>
2021-09-14 20:53:30 +0100

    add acceptanceTest

--- src/main/java/com/codurance/primeFactors/PrimeFactors.java
+++ src/main/java/com/codurance/primeFactors/PrimeFactors.java
@@ -8,16 +8,16 @@ import java.util.List;
 public class PrimeFactors {
   public static List<Integer> of(int n) {
     final List<Integer> factors = Lists.newArrayList();
-    if (n > 1) {
-      if (n % 2 == 0){
-        factors.add(2);
-        n /= 2;
+    int divisor = 2;
+    while (n > 1) {
+      while (n % divisor == 0){
+        factors.add(divisor);
+        n /= divisor;
       }
 
-      if (n > 1){
-        factors.add(n);
-      }
+      divisor++;
     }
+
     return factors;
   }
 }
--- src/test/java/com/codurance/primeFactors/PrimeFactorsTest.java
+++ src/test/java/com/codurance/primeFactors/PrimeFactorsTest.java
@@ -31,4 +31,19 @@ public class PrimeFactorsTest {
   void of_6Is2_3() {
     assertThat(PrimeFactors.of(6), is(ImmutableList.of(2, 3)));
   }
+
+  @Test
+  void of_8Is_2_2_2() {
+    assertThat(PrimeFactors.of(8), is(ImmutableList.of(2, 2, 2)));
+  }
+
+  @Test
+  void of_9Is_3_3_3() {
+    assertThat(PrimeFactors.of(9), is(ImmutableList.of(3, 3)));
+  }
+
+  @Test
+  void of_acceptanceTest() {
+    assertThat(PrimeFactors.of(2*2*2*3*3*5*7*11), is(ImmutableList.of(2, 2, 2, 3, 3, 5, 7, 11)));
+  }
 }
